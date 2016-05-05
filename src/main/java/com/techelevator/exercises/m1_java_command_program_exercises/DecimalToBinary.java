b8858a3d366c0a7b8617949c003ff4f3782a125c
Craig Castelaz <craig@techelevator.com>
2016-05-05 16:55:17 -0400

    Consolidated all m1-java command line programs into one repository

---
+++ src/main/java/com/techelevator/exercises/m1_java_command_program_exercises/DecimalToBinary.java
@@ -0,0 +1,20 @@
+package com.techelevator.exercises.m1_java_command_program_exercises;
+
+/*
+Write a command line program which accepts a series of decimal integer values as command line arguments, 
+and displays each decimal value as itself and its binary equivalent
+
+$ DecimalToBinary 460 8218 1 31313 987654321
+460 in binary is 111001100
+8218 in binary is 10000000011010
+1 in binary is 1
+31313 in binary is 111101001010001
+987654321 in binary is 111010110111100110100010110001
+*/
+public class DecimalToBinary {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/com/techelevator/exercises/m1_java_command_program_exercises/Fibonacci.java
@@ -0,0 +1,20 @@
+package com.techelevator.exercises.m1_java_command_program_exercises;
+
+/*
+ The Fibonacci numbers are the integers in the following sequence:  
+	0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+ By definition, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
+ 
+ Write a command line program which displays a Fibonacci sequence to the console. The program accepts an integer value as a command line 
+ argument which specifies when to terminate the Fibonacci sequence, and quit the program.
+  
+ $ java Fiboncci 25
+ 0, 1, 1, 2, 3, 5, 8, 13, 21
+ */
+public class Fibonacci {
+
+	public static void main(String[] args) {
+
+	}
+
+}
